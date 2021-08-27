36efbfd38dcad8eefcaddeb1b999201727dec1e6
Paul Stocks <paul.stocks@gmail.com>
2021-08-27 13:18:19 -0700

    Added Git Hash to about

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
 import Header from "../components/header";
 function About() {
+  let COMMIT_SHA = process.env.VERCEL_GITHUB_COMMIT_SHA;
   return (
     <>
       <Header />
       <h2>About whatever</h2>
+      {COMMIT_SHA ? <h6>COMMIT_SHA</h6> : null} 
     </>
   );
 }
