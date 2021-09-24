105cf7bcfadffe4e49012d39ff7b4819fe8384ca
Paul Stocks <paul.stocks@gmail.com>
2021-09-24 14:38:20 -0700

    season fix

--- libs/getSquad.js
+++ libs/getSquad.js
@@ -6,12 +6,12 @@ const handler = async (req, res) => {
     const docs = await req.db
 
       .collection("dates")
-      .find({ dateId: parseInt(req.query.dateId) })
+      .find({ dateId: parseInt(req.query.dateId), season: req.query.season })
       .project({ squad: 1, _id: 0 })
       .toArray();
 
     console.log("founda doc", docs[0]);
-    return {message: "ok", doc: docs[0]};
+    return { message: "ok", doc: docs[0] };
   } catch (error) {
     res.json("Error: " + error.toString());
   }
