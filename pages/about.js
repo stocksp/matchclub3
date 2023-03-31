660fa89834075c05cb65b378097e013433be3bd0
Paul Stocks <paul.stocks@gmail.com>
2023-03-31 11:46:42 -0700

    Testing Form

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,26 @@
-import Header from "../components/header";
+import Header from "../components/header"
 function About() {
-  let COMMIT_SHA = process.env.buildId;
+  let COMMIT_SHA = process.env.buildId
   return (
     <>
       <Header />
       <h2>About whatever</h2>
-      <h6>Build: {COMMIT_SHA}</h6> 
+      <h6>Build: {COMMIT_SHA}</h6>
+      <form action="https://dev.skeletonforms.com/api/formPost?key=10001" method="POST">
+        <label>
+          Your email address:
+          <input type="email" name="email" />
+        </label>
+        <br />
+        <label>
+          Your message:
+          <textarea name="message"></textarea>
+        </label>
+        <br />
+        <button type="submit">Send</button>
+      </form>
     </>
-  );
+  )
 }
 
-export default About;
+export default About
