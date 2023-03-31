fdbd6a612e3ffcd8bf756353ac60aae56a6f101f
Paul Stocks <paul.stocks@gmail.com>
2023-03-31 11:51:29 -0700

    Removed Form Test

--- pages/about.js
+++ pages/about.js
@@ -6,19 +6,6 @@ function About() {
       <Header />
       <h2>About whatever</h2>
       <h6>Build: {COMMIT_SHA}</h6>
-      <form action="https://dev.skeletonforms.com/api/formPost?key=10001" method="POST">
-        <label>
-          Your email address:
-          <input type="email" name="email" />
-        </label>
-        <br />
-        <label>
-          Your message:
-          <textarea name="message"></textarea>
-        </label>
-        <br />
-        <button type="submit">Send</button>
-      </form>
     </>
   )
 }
